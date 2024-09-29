826f73b1d5b74d29854c8b461b36706962eff228
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-09-29 14:42:19 +0800

    Fix bugs in code style

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.Messages;

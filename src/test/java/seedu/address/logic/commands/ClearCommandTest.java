4b60078b236ebf9ff240bb517c65cd3b81f3cdbd
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-10-16 16:50:20 +0800

    Update clear command test

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -14,19 +15,55 @@ public class ClearCommandTest {
 
     @Test
     public void execute_emptyAddressBook_success() {
+        ClearCommand command = new TestClearCommand(true);
         Model model = new ModelManager();
         Model expectedModel = new ModelManager();
 
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(command, model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
+        ClearCommand command = new TestClearCommand(true);
         Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel.setAddressBook(new AddressBook());
 
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(command, model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+
+    @Test
+    public void execute_nonEmptyAddressBook_cancel() {
+        ClearCommand command = new TestClearCommand(false);
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+        assertCommandSuccess(command, model, ClearCommand.MESSAGE_CANCEL, expectedModel);
+    }
+
+    /**
+     * A helper class to simulate user confirmation input,
+     * avoiding the need for actual GUI interaction during testing.
+     */
+    private static class TestClearCommand extends ClearCommand {
+        private final boolean confirmClear;
+
+        public TestClearCommand(boolean confirmClear) {
+            this.confirmClear = confirmClear;
+        }
+        
+        @Override
+        public CommandResult execute(Model model) {
+            requireNonNull(model);
+
+            if (confirmClear) {
+                model.setAddressBook(new AddressBook());
+                return new CommandResult(MESSAGE_SUCCESS);
+            } else {
+                return new CommandResult(MESSAGE_CANCEL);
+            }
+        }
     }
 
 }

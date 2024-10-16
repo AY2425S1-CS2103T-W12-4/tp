1790493798b346c62bf7b9472d0a2448182f308e
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-10-16 16:36:47 +0800

    Add confirmation for clear command

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,8 +2,15 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+
+
 
 /**
  * Clears the address book.
@@ -12,12 +19,25 @@ public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-
+    public static final String MESSAGE_CANCEL = "Clear operation has been cancelled.";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Required");
+        alert.setHeaderText("Are you sure you want to clear the address book?");
+        alert.setContentText("Click OK to confirm, or Cancel to abort.");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            model.setAddressBook(new AddressBook());
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            return new CommandResult(MESSAGE_CANCEL);
+        }
+
     }
 }
